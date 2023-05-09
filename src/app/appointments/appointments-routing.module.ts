777bdf454fb442ae692cb1a82c4025ed40fb877a
manuela-torres <manuela.torres@sophossolutions.com>
2023-05-09 16:04:59 -0500

    Rutas definidas

--- src/app/appointments/appointments-routing.module.ts
+++ src/app/appointments/appointments-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from '../pages/layout/layout.component';
+import { AppointmentsListComponent } from './appointments-list/appointments-list.component';
+import { CreateAppointmentComponent } from './create-appointment/create-appointment.component';
+import { DeleteAffiliateComponent } from '../affiliates/delete-affiliate/delete-affiliate.component';
+import { DelateAppointmentComponent } from './delate-appointment/delate-appointment.component';
+import { EditAppointmentComponent } from './edit-appointment/edit-appointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LayoutComponent},
+
+    //{path:'appointments-list', component: AppointmentsListComponent},
+    {path:'create-appointment', component: CreateAppointmentComponent},
+    {path:'delete-appointment', component: DelateAppointmentComponent},
+    {path:'editappointment/:id', component: EditAppointmentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
