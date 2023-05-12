b29fac7d6ee0fa661e08021dd6b8fb94de27d02c
manuela-torres <manuela.torres@sophossolutions.com>
2023-05-12 16:35:05 -0500

    getAppointmentsByIdAffiliate (W/ Gus)

--- src/app/appointments/appointments-list/appointments-list.component.ts
+++ src/app/appointments/appointments-list/appointments-list.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
 import { Appointment } from 'src/app/models/appointment';
-import { AffiliatesService } from 'src/app/services/affiliates.service';
 import { AppointmentsService } from 'src/app/services/appointments.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
 
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', },
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', },
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', },
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', },
-
-];
 
 @Component({
   selector: 'app-appointments-list',
@@ -26,7 +11,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class AppointmentsListComponent {
   displayedColumns: string[] = ['id', 'date', 'hora', 'idTest','idAffiliate','option'];
-  dataSource = ELEMENT_DATA;
+
 
   public appointments: Appointment[];
 
